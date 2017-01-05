e9d02d778cc9fa4d042a14359dc037eb4eea2f2d
zxh1993 <471207747@qq.com>
2017-01-05 15:41:33 +0800

    修改网卡

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
     CPcap cap;
     CPcap::findAllDevs(); //找所有设备
     cap.setCapNums(-1); //设置捕获数,-1 一直捕获
-    cap.setDevice("eno1"); // 设置网卡
+    cap.setDevice("default"); // 设置网卡
     cap.setFilterExp("udp or tcp"); //过滤规则
     int count = 0; //包计数
     cap.start(CPcap::pcapHandler, (u_char *) &count);
